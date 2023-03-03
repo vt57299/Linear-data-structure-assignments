80027ccf79a999bb470966ef2c5d2797823a74dc
Vivek <84849703+vt57299@users.noreply.github.com>
2023-03-03 23:46:11 +0530

    Update Question5.py

--- Question5.py
+++ Question5.py
@@ -8,14 +8,13 @@ Only one disk can be moved at a time.
 Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack i.e. a disk can only be moved if it is the uppermost disk on a stack.
 No disk may be placed on top of a smaller disk.'''
 
-def T_O_H(n,source,auxilaiary,destination):
+def T_O_H(n,source,auxiliary,destination):
     if n == 1:
         print("moved disk 1 from source", source,"to destination", destination)
         return 
-    T_O_H(n-1,source,auxilaiary,destination)
-    print("moved disk",n, )
+        
+    T_O_H(n-1, source, auxiliary, destination)
+    print ("Move disk",n,"from source",source,"to destination",destination)
+    T_O_H(n-1, auxiliary, destination, source)
 
-
-
-
-T_O_H(1,"A","B","C")
+print(T_O_H(3,"A","B","C"))
